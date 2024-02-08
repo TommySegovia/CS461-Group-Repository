f25b82303a61d2e2e1482a33ac4d915af6f9c018
Bentoboxing - Patrick <installby3@gmail.com>
2024-02-08 00:01:01 -0800

    finish up the down/up.sql and create a basic seed.sql

--- GroupWork/Milestone-4/down.sql
+++ GroupWork/Milestone-4/down.sql
@@ -1,6 +1,6 @@
 -- Drop foreign key constraints
 ALTER TABLE [FitnessDataEntry] DROP CONSTRAINT [FK_FitnessDataEntry_Climber_ID];
-ALTER TABLE [FitnessTest] DROP CONSTRAINT [FK_FitnessDataEntry_Test_ID];
+ALTER TABLE [FitnessDataEntry] DROP CONSTRAINT [FK_FitnessDataEntry_Test_ID];
 
 -- Drop tables
 DROP TABLE [FitnessDataEntry];
--- GroupWork/Milestone-4/up.sql
+++ GroupWork/Milestone-4/up.sql
@@ -1,7 +1,10 @@
+
+
 CREATE TABLE [Climber] (
   [ID]        int           PRIMARY KEY IDENTITY(1, 1),
-  [Email]     nvarchar(255) NOT NULL,
-  [Password]  nvarchar(255) NOT NULL
+  [ASPNetIdentityId] nvarchar(450) NOT NULL,
+  [FirstName]     nvarchar(255) NOT NULL,
+  [LastName]  nvarchar(255) NOT NULL
 );
 
 CREATE TABLE [FitnessTest] (
@@ -21,5 +24,5 @@ CREATE TABLE [FitnessDataEntry] (
 ALTER TABLE [FitnessDataEntry] ADD CONSTRAINT [FK_FitnessDataEntry_Climber_ID] 
   FOREIGN KEY ([ClimberID]) REFERENCES [Climber] ([ID]);
 
-ALTER TABLE [FitnessTest] ADD CONSTRAINT [FK_FitnessDataEntry_Test_ID]
-  FOREIGN KEY ([ID]) REFERENCES [FitnessDataEntry] ([TestID]);
+ALTER TABLE [FitnessDataEntry] ADD CONSTRAINT [FK_FitnessDataEntry_Test_ID]
+  FOREIGN KEY ([TestID]) REFERENCES [FitnessTest] ([ID]);
