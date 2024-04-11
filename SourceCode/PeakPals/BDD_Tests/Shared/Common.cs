8887f9b3b0010b115878e3d38d2ba534fd09e8e1
Tommy <tsegovia20@mail.wou.edu>
2024-04-10 21:26:36 -0700

    Added BDD testing for TBD-37

---
+++ SourceCode/PeakPals/BDD_Tests/Shared/Common.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakPals_BDD_Tests.Shared
+{
+    // Sitewide definitions and useful methods
+    public class Common
+    {
+        public const string BaseUrl = "http://localhost:5044";     // copied from launchSettings.json
+        
+
+        // File to store browser cookies in
+         public const string CookieFile = "..\\..\\..\\..\\..\\StandupsCookies.txt";
+
+        // Page names that everyone should use
+        // A handy way to look these up
+        public static readonly Dictionary<string, string> Paths = new()
+        {
+            { "Home" , "/" },
+            { "Report", "/Report/Report" },
+            { "Record", "/Record/Record" },
+            { "Login", "/Identity/Account/Login" },
+            { "Register", "/Identity/Account/Register" },
+            { "Manage", "/Identity/Account/Manage" },
+            { "Logout", "/Identity/Account/Logout" },
+            { "Privacy", "/Privacy" },
+            { "Error", "/Home/Error" },
+            { "NotFound", "/Home/NotFound" },
+            { "ServerError", "/Home/ServerError" }
+        };
+
+        public static string PathFor(string pathName) => Paths[pathName];
+        public static string UrlFor(string pathName) => BaseUrl + Paths[pathName];
+    }
+}
