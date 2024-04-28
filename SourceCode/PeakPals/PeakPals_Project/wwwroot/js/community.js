b53582d8409ab9984b4578c1123cf2a756e0043b
Tommy <tsegovia20@mail.wou.edu>
2024-04-27 23:08:25 -0700

    Added basic searching for community groups

--- SourceCode/PeakPals/PeakPals_Project/wwwroot/js/community.js
+++ SourceCode/PeakPals/PeakPals_Project/wwwroot/js/community.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function()
     console.log("Index page created.")
     const searchButton = document.getElementById("search-button");
     searchButton.addEventListener("click", searchButtonClicked, false);
+
+    const communityGroupSearchButton = document.getElementById("community-group-search-button");
+    communityGroupSearchButton.addEventListener("click", communityGroupSearchButtonClicked, false);
 });
 
 async function searchButtonClicked(e)
@@ -80,4 +83,72 @@ async function searchButtonClicked(e)
 
     })
     
+}
+
+async function communityGroupSearchButtonClicked(e){
+    //searches for community groups and populates the search results div with the results by building the template for each group using the groups name and description
+    console.log("community group search button clicked.")
+    const searchResultsDiv = document.getElementById("community-group-search-results");
+    searchResultsDiv.innerHTML = "";
+    const errorMessage = document.createElement("p");
+    const validationWarning = document.getElementById("community-validation-warning");
+    validationWarning.innerHTML = "";
+
+    const searchInput = document.getElementById("community-group-search-input");
+    const groupName = searchInput.value;
+    const isValid = /^[a-z0-9]+$/i.test(groupName);
+
+    if(!isValid)
+    {
+        errorMessage.textContent = "Invalid input. Please enter a search term with valid characters.";
+        errorMessage.classList.add("error-message");
+        validationWarning.appendChild(errorMessage);
+        return;
+    }
+
+    if (groupName === "")
+    { 
+        errorMessage.textContent = "Invalid input. Please enter a search term.";
+        errorMessage.classList.add("error-message");
+        validationWarning.appendChild(errorMessage);
+        return;
+    }
+
+    const url = `/api/community/search/group/${groupName}`;
+    console.log(url);
+    const response = await fetch(url);
+    console.log(response);
+    if (!response.ok)
+    {
+        errorMessage.textContent = "Sorry, the response returned an error."
+        errorMessage.classList.add("error-message");
+        validationWarning.appendChild(errorMessage);
+        return;
+    }
+    const communityGroups = await response.json();
+    if (communityGroups.length === 0) {
+        errorMessage.textContent = "Sorry, no community group exists with that name."
+        errorMessage.classList.add("error-message");
+        validationWarning.appendChild(errorMessage);
+        return;
+    }
+    console.log(communityGroups);
+
+    const resultTemplate = document.getElementById("community-group-template");
+
+    communityGroups.forEach(communityGroup => 
+    {
+        //creates a new clone of the template for each community group and populates the template with the groups name and description
+        const clone = resultTemplate.content.cloneNode(true);
+        const groupLink = clone.getElementById('group-link-name');
+        //groupLink.href = `/Community/Group/${communityGroup.id}`;
+        groupLink.innerHTML = communityGroup.name;
+
+        const groupDescription = clone.getElementById('group-description');
+        groupDescription.innerHTML = communityGroup.description;
+
+        searchResultsDiv.appendChild(clone);
+        console.log("created a new community group object to show.");
+
+    })
 }
